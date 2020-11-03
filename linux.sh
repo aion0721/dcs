67266bdf700b3a8057ae05c63a38b21146d3e5d5
aion0721 <rudolf.aion@gmail.com>
2020-11-03 18:50:46 +0900

    Category Checking

--- linux.sh
+++ linux.sh
@@ -6,13 +6,17 @@ IFS=,
 
 # 一時ディレクトリ生成
 sourcePath=`mktemp -d`
-
+# ファイル名定義
 fileName=$(date "+%Y%m%d_%H%M%S")_dcs
 
 # リストファイル定義
 textlist=$(cat <<EOS
 1-1,test1,cmd,echo a
 1-2,test2,cmd,echo b
+1-3,ifconfig,cmd,ifconfig
+2-1,secure,path,/var/log/messages
+2-2,syslog,path,/var/log/syslog
+2-3,ruby-helper,path,/root/spec/spec_helper.rb
 EOS
 )
 
@@ -21,6 +25,12 @@ echo $textlist > $sourcePath/textlist.txt
 # 行ループ
 while read -a arr; do
 
+# 変数定義
+snum=${arr[0]}
+stitle=${arr[1]}
+category=${arr[2]}
+args=${arr[3]}
+
 echo "-------- Loop -------"
 echo "arr[*]: ${arr[*]}"
 echo "arr[0]: ${arr[0]}"
@@ -28,6 +38,18 @@ echo "arr[1]: ${arr[1]}"
 echo "arr[2]: ${arr[2]}"
 echo "arr[3]: ${arr[3]}"
 echo "arr[d]: $(eval ${arr[3]})"
+
+# 処理判断
+# cmdの場合コマンド処理
+if [ ${category} = "cmd" ]; then
+	res=$(eval ${args})
+	echo $res > ${sourcePath}/${snum}_${stitle}.txt
+
+# pathの場合ファイルコピー
+elif [ ${category} = "path" ]; then
+	mkdir $sourcePath/${snum}_${stitle}
+	cp -p ${args} $sourcePath/${snum}_${stitle}/
+fi
 
 done < <(echo "$textlist")
 
